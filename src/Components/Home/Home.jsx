c0176309ac795760dc1aa5a3bd97375c99ef07a1
lokakshi <adityalokakshijee@gmail.com>
2023-01-18 16:20:08 +0530

    modification in admin side panel

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,18 +1,33 @@
 import React from 'react'
 import { TopBar } from '../TopBar/TopBar'
 import { UserForm } from '../UserForm/UserForm'
+import { SideBar } from '../SideBar/SideBar'
 import './Home.css'
+
 export const Home = () => {
   return (
+   
     <div className="home"> 
-        <TopBar/>
+        <TopBar className='navbar-home'/> 
+    <div className='Switch-div'>
+        <div className='graph-div'>
+             
+        </div>
+        <div className='companydetail-div'>
 
-      <div className='Createuser'>
-       <UserForm/> 
-      </div>
-        
+        </div>
+        <div className='div1'>
+
+        </div>
+        <div className='div2'>
+
+        </div>
     </div>
+    <div className='Sidebar-div'>
 
-        
+    </div>  
+
+    </div>  
+   
   )
 }
